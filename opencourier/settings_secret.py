b785e5b99ef4ef57a09009adc798a5e4926d4d3e
1Luc1 <lukas.aigner@gmail.com>
2018-09-18 14:01:41 +0200

    Create settings_secret.py

---
+++ opencourier/settings_secret.py
@@ -0,0 +1,5 @@
+import dj_database_url
+DATABASES = { 'default': dj_database_url.config() }
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'l7tfk!#2e=h==+=x)28xj%m=jy=+gqcbz_95@g!_m1h3i3)p@!'
