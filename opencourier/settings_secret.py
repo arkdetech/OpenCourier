11a88a5097de58c1a27cefb28ba7da35a82b2366
1Luc1 <lukas.aigner@gmail.com>
2018-09-18 14:41:32 +0200

    Create settings_secret.py

---
+++ opencourier/settings_secret.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
